ae526fc79ed65d1db2463707ccfdc4bd1ceac805
bradyoo12 <yoohoony@hotmail.com>
2024-02-19 01:14:57 +0900

    oh yeah

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # https://www.youtube.com/watch?v=dfeM91JG3oQ
+# Automatic deployment on push of any application to VPS server by using Github workflows action https://www.youtube.com/watch?v=V2YYhGn3MGo
 from flask import Flask, jsonify
 app=Flask(__name__)
 
 @app.route("/")
 def root():
-    return jsonify({"Message":"Hello from Flask!!"})
+    return jsonify({"Message":"Hello from Flask!! oh yeah"})
 
 if __name__=="__main__":
     app.run(debug=True,host="0.0.0.0",port=8080)
